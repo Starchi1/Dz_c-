efe4f6af957eb6698bf195a17ca667ecf90f2194
Starchi1 <91777726+Starchi1@users.noreply.github.com>
2023-09-06 22:38:21 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,14 +10,44 @@ int main() {
 	cin >> c;
 	if (b == 0) {
 		if (c != 0) {
-			cout << "íåò ðåøåíèÿ";
+			cout << "Ã­Ã¥Ã² Ã°Ã¥Ã¸Ã¥Ã­Ã¨Ã¿";
 		}
 		if (c == 0) {
-			cout << "ëþáîé x";
+			cout << "Ã«Ã¾Ã¡Ã®Ã© x";
 		}
 	}
 	else {
 		cout << -c / b;
 	}
+	return 0;
+}
+#include <iostream>
+#include <clocale>
+#include <cmath>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a;
+	int b;
+	int c;
+	double d;
+	double x1;
+	double x2;
+	
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	d = pow(b,2)-4*a*c;
+	if (d == 0) {
+	    cout << -b/2*a;
+	}
+	if (d > 0){
+	    cout << (-b+pow(d,0.5)) / (2*a); cout << " "; cout << (-b-pow(d,0.5)) / (2*a);
+	}
+	else{
+	    cout << "Ð½ÐµÑ‚ ÐºÐ¾Ñ€Ð½ÐµÐ¹";
+	}
+
 	return 0;
 }
